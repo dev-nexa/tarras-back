435ebaacb9bb4d13a5a3aa1ccc8b2db5d6f8dda7
zaitouny <ii.zaitouny@gmail.com>
2024-11-03 21:29:21 +0300

    anas

--- models/external_orders.sql
+++ models/external_orders.sql
@@ -6,6 +6,7 @@ CREATE TABLE external_orders (
     region_price DECIMAL(10, 2) NOT NULL DEFAULT 0,
     order_status ENUM('بانتظار الاتصال', 'جاري التحضير', 'جاري التوصيل', 'تم الاستلام') DEFAULT 'بانتظار الاتصال',
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    note VARCHAR(256) DEFAULT 'لا يوجد ملاحظات',
     FOREIGN KEY (customer_id) REFERENCES customers(id) ON DELETE CASCADE,
     FOREIGN KEY (region_id) REFERENCES locations(id) ON DELETE CASCADE
 );
--- models/order_items.sql
+++ models/order_items.sql
@@ -6,6 +6,7 @@ CREATE TABLE order_items (
     order_type ENUM('خارجي', 'داخلي'),
     total_amount DECIMAL(10, 2) NOT NULL,
     issued_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    note VARCHAR(256) DEFAULT 'لا يوجد ملاحظات',
     FOREIGN KEY (external_order_id) REFERENCES external_orders(id) ON DELETE CASCADE,
     FOREIGN KEY (internal_order_id) REFERENCES internal_orders(id) ON DELETE CASCADE,
     FOREIGN KEY (product_id) REFERENCES products(id) ON DELETE CASCADE
